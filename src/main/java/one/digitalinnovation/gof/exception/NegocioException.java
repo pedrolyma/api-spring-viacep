f4b9a8d7240084d039305b16d22f389c141dbb84
pedro lima <pelyma33@gmail.com>
2021-09-30 15:43:54 -0300

    commit do projeto spring usando viacep

---
+++ src/main/java/one/digitalinnovation/gof/exception/NegocioException.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.gof.exception;
+
+public class NegocioException extends RuntimeException {
+
+    private static final long serialVersionUID =  1L;
+
+    public NegocioException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,78 @@
+package one.digitalinnovation.gof.service.impl;
+
+import java.util.Optional;
+
+import one.digitalinnovation.gof.exception.EntidadeNaoEncontradaException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.repository.ClienteRepository;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.repository.EnderecoRepository;
+import one.digitalinnovation.gof.service.ClienteService;
+import one.digitalinnovation.gof.service.ViaCepService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+	// Singleton: Injetar os componentes do Spring com @Autowired.
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
+	
+	// Strategy: Implementar os métodos definidos na interface.
+	// Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Cliente Nao Encontrado"));
+//		Optional<Cliente> cliente = clienteRepository.findById(id);
+//		return cliente.get();
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		salvarClienteComCep(cliente);
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		// Buscar Cliente por ID, caso exista:
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		} else {
+			new EntidadeNaoEncontradaException("Cliente Nao Encontrado");
+		}
+	}
+
+	@Override
+	public void deletar(Long id) {
+		clienteRepository.deleteById(id);
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		// Verificar se o Cep já existe.
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			// Caso não exista, buscar no ViaCEP e persistir o retorno.
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		// Inserir Cliente, vinculando o Endereco (novo ou existente).
+		clienteRepository.save(cliente);
+	}
+
+}
